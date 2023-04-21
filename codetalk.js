1b40e15d3d92def4f31894b48df1a36bf7f298df
Aishah Isom <aishahisom@AdminisatorsMBP-18.lan>
2023-04-21 16:08:38 -0400

    first commit

---
+++ codetalk.js
@@ -0,0 +1,11 @@
+const n = 20;
+
+for (let i = 1; i <= n; i++) {
+  console.log("I love Aishah");
+}
+
+//for(initialization; condition; iteration)
+//console.log( will print out 5 times and on the fifth time the program will stop)
+//The program stops because the variable i becomes 6 which will make the for loop
+// false because i will no longer be less than or equal to our original variable 5
+//
